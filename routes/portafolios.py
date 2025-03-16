12ae1dd12345708019dc7f9bcd015238397aaf4a
Marcus <shavo08kof@gmail.com>
2025-03-16 20:09:21 +0100

    Portafolio configurado

--- routes/portafolios.py
+++ routes/portafolios.py
@@ -6,20 +6,24 @@ portafolios = Blueprint('portafolios', __name__, template_folder='templates/port
 # Rutas para cada portfolio
 @portafolios.route('/Coches')
 def coches():
-    return render_template('coches.html')  # plantilla en templates/portafolios/coches.html
+    return render_template('portafolios/coches.html')  # plantilla en templates/portafolios/coches.html
 
-@portafolios.route('/Retratos')
-def retratos():
-    return render_template('retratos.html')  # plantilla en templates/portafolios/retratos.html
+@portafolios.route('/Retratos_Femeninos')
+def retratosm():
+    return render_template('portafolios/retratosm.html')  # plantilla en templates/portafolios/retratos.html
+
+@portafolios.route('/Retratos_Masculinos')
+def retratosh():
+    return render_template('portafolios/retratosh.html')  # plantilla en templates/portafolios/retratos.html
 
 @portafolios.route('/Comida')
 def comida():
-    return render_template('comida.html')  # plantilla en templates/portafolios/comida.html
+    return render_template('portafolios/comida.html')  # plantilla en templates/portafolios/comida.html
 
 @portafolios.route('/Bodas')
 def bodas():
-    return render_template('bodas.html')  # plantilla en templates/portafolios/bodas.html
+    return render_template('portafolios/bodas.html')  # plantilla en templates/portafolios/bodas.html
 
 @portafolios.route('/Urbana')
 def urbana():
-    return render_template('urbana.html')  # plantilla en templates/portafolios/urbana.html
+    return render_template('portafolios/urbana.html')  # plantilla en templates/portafolios/urbana.html
