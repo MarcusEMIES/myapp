200142e769349c9f9a0578cf196edf3b1a51d651
Marcus <shavo08kof@gmail.com>
2025-03-17 21:30:03 +0100

    Corregido Padding de Portfoloios

--- static/javaScript.js
+++ static/javaScript.js
@@ -1,16 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
   // Obtener el modal y la imagen dentro del modal
   var modalImage = document.getElementById('modalImage');
-
-  // Obtener todas las imágenes dentro de las tarjetas
   var imageElements = document.querySelectorAll('img[data-bs-image]');
 
-  // Agregar el evento de clic a cada imagen
   imageElements.forEach(function(imageElement) {
-    imageElement.addEventListener('click', function() {
-      // Actualizar la imagen en el modal con la fuente de la imagen clickeada
-      var imageUrl = imageElement.getAttribute('data-bs-image');
-      modalImage.setAttribute('src', imageUrl);
-    });
+     imageElement.addEventListener('click', function() {
+        var imageUrl = imageElement.getAttribute('data-bs-image');
+        if (imageUrl) {
+           modalImage.setAttribute('src', imageUrl);
+        } else {
+           console.error("No se encontró la URL de la imagen");
+        }
+     });
   });
 });
