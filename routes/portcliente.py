d00dab5c183a5c7dc99a5ee68159c29657ee672f
Marcus Hinestroza <shavo08kof@gmail.com>
2025-03-29 18:30:46 +0100

    Corregido modal en modo telefono vista

--- routes/portcliente.py
+++ routes/portcliente.py
@@ -1,6 +1,10 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash, session
 from flask_login import login_required, current_user
 from models.products import Product
+from flask import  abort
+from io import BytesIO
+from zipfile import ZipFile
+
 
 portafolio = Blueprint('portafolio', __name__)
 
@@ -67,3 +71,32 @@ def descargar_todo():
 
     memory_file.seek(0)
     return send_file(memory_file, download_name='portafolio.zip', as_attachment=True)
+
+
+#Ruta para descargar el book completo
+
+@portafolio.route('/descargar_book/<int:book_id>')
+@login_required
+def descargar_todo_por_book(book_id):
+    producto = Product.query.get_or_404(book_id)
+    if producto.user_id != current_user.id and current_user.role != 'admin':
+        abort(403)
+
+    # Archivos a incluir
+    files = []
+    if producto.image_urls:
+        files += [os.path.join('static', img.strip()) for img in producto.image_urls.split(';')]
+    if producto.video_urls:
+        files += [os.path.join('static', vid.strip()) for vid in producto.video_urls.split(';')]
+
+    # Crear el ZIP
+    zip_stream = BytesIO()
+    with ZipFile(zip_stream, 'w') as zipf:
+        for file in files:
+            zipf.write(file, os.path.basename(file))
+    zip_stream.seek(0)
+
+    return send_file(zip_stream,
+                     mimetype='application/zip',
+                     as_attachment=True,
+                     download_name=f'{producto.name}_portafolio.zip')
